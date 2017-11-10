7f7243f08e663ca4ed362f2ae48bd91035b817b5
rattios <rattio.tech@gmail.com>
2017-11-10 01:38:27 -0400

    slider

--- md-angular-v1.4.1/src/app/slider/slider.component.ts
+++ md-angular-v1.4.1/src/app/slider/slider.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams  } from '@angular/common/http';
 import { FormGroup, FormArray, FormBuilder, Validators  } from '@angular/forms';
 
+declare const $: any;
 
 @Component({
   selector: 'app-slider',
@@ -17,6 +18,7 @@ export class SliderComponent implements OnInit {
 	public loading=false;
 	public sliderForm: FormGroup;
 	public sliderF:any;
+	public i:any;
 
 	constructor(private http: HttpClient, private builder: FormBuilder) {
 	  	this.sliderForm = this.builder.group({
@@ -33,13 +35,25 @@ export class SliderComponent implements OnInit {
 
 	ngOnInit() {
 	  	this.loading=true;
-      	this.http.get('http://apimanappger.internow.com.mx/api/public/datos')
+      	this.http.get('http://apimanappger.internow.com.mx/api/public/slider')
            .subscribe((data)=> {
            	   console.log(data);
            	   this.sliderF=data;
            	   this.sliderF=this.sliderF.slider;
            	   console.log(this.sliderF);
+           	   console.log(this.sliderF.length);
                this.loading=false;
+
+               const control= <FormArray>this.sliderForm.controls["slider"];
+
+               for (var i = 0; i < this.sliderF.length-1; i++) {
+            		control.push(this.slider());
+            	}
+
+               for (var i = 0; i < this.sliderF.length; i++) {
+            		(<FormArray>this.sliderForm.controls['slider']).at(i).patchValue({img: this.sliderF[i].img });
+            		(<FormArray>this.sliderForm.controls['slider']).at(i).patchValue({pos: i+1 });
+            	}
             });
 	}
 
@@ -53,6 +67,85 @@ export class SliderComponent implements OnInit {
         }
     }
 
+    remover(i){
+        const control= <FormArray>this.sliderForm.controls["slider"];
+        var index=control.value.length-1;
+        control.removeAt(index);
+    }
+
+    guardar(item){
+    	console.log(JSON.stringify(item.value.slider));
+    	var send={
+    		slider:JSON.stringify(item.value.slider)
+    	};
+    	console.log(send);
+        this.http.put('http://apimanappger.internow.com.mx/api/public/datos/1',send)
+         .toPromise()
+         .then(
+           data => { // Success
+             console.log(data);
+             this.showNotification('top','center','Actualizado con exito',2);
+             this.loading=false;
+             this.reset();
+           },
+           msg => { // Error
+             console.log(msg);
+             this.showNotification('top','center','Ha ocurrido un error' + JSON.stringify(msg.error),4);
+             this.loading=false;
+             this.reset();
+        });
+    }
+
+    reset(){
+    	this.loading=true;
+      	this.http.get('http://apimanappger.internow.com.mx/api/public/slider')
+           .subscribe((data)=> {
+           	   console.log(data);
+           	   this.sliderF=data;
+           	   this.sliderF=this.sliderF.slider;
+           	   console.log(this.sliderF);
+           	   console.log(this.sliderF.length);
+               this.loading=false;
+
+               this.sliderForm = this.builder.group({
+		            slider: this.builder.array([this.slider()])
+			    })
+               const control= <FormArray>this.sliderForm.controls["slider"];
+
+               for (var i = 0; i < this.sliderF.length-1; i++) {
+            		control.push(this.slider());
+            	}
+
+               for (var i = 0; i < this.sliderF.length; i++) {
+            		(<FormArray>this.sliderForm.controls['slider']).at(i).patchValue({img: this.sliderF[i].img });
+            		(<FormArray>this.sliderForm.controls['slider']).at(i).patchValue({pos: i+1 });
+            	}
+            });
+    }
+
+    showNotification(from, align, mensaje,colors){
+          const type = ['','info','success','warning','danger'];
+
+          const color = colors;
+          
+          $.notify({
+              icon: "notifications",
+              message: mensaje
+
+          },{
+              type: type[color],
+              timer: 4000,
+              placement: {
+                  from: from,
+                  align: align
+              }
+          });
+    }
+    setI(i){
+    	console.log(i);
+    	this.i=i;
+    }
+
   	uploadFile: any;
     hasBaseDropZoneOver: boolean = false;
     options: Object = {
@@ -60,11 +153,14 @@ export class SliderComponent implements OnInit {
     };
     sizeLimit = 2000000;
 	handleUpload(data): void {
+
       if (data && data.response) {
         data = JSON.parse(data.response);
         this.uploadFile = data;
         this.loading=false;
-        this.sliderForm.patchValue({img: 'http://vivomedia.com.ar/cuetociasrl/uploads/'+this.uploadFile.generatedName });
+        //this.sliderForm.patchValue({img: 'http://apimanappger.internow.com.mx/uploads/'+this.uploadFile.generatedName });
+      	(<FormArray>this.sliderForm.controls['slider']).at(this.i).patchValue({img: 'http://apimanappger.internow.com.mx/uploads/'+this.uploadFile.generatedName});
+      	console.log(this.sliderForm);
       }
     }
    
